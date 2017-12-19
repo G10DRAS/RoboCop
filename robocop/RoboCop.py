3763234d6fc74adb14de6a84b01eb30930e51da8
G10DRAS <olxboss@gmail.com>
2017-12-19 07:52:09 +0530

    Update RoboCop.py

--- robocop/RoboCop.py
+++ robocop/RoboCop.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8-*-
 
-# Copyright 2017 g10dras.
+# Copyright 2017 g10dras
 __author__ = 'g10dras'
+__version__ = "0.0.7"
+
+
+import os
+import sys
+import logging
+import yaml
+import argparse
 
 from asciimatics.effects import Cycle, Stars
 from asciimatics.renderers import FigletText
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
+
+parser = argparse.ArgumentParser(description='RoboCop Voice Control Center')
+parser.add_argument('--consoleinput', action='store_true',
+                    help='Use text input instead of a real microphone')
+parser.add_argument('--yamlconfig', action='store_true',
+                    help='Prefer YAML config over SQLite3DB config')
+parser.add_argument('--configdb', action='store_true',
+                    help='Prefer SQLite3DB over YAML config')
+parser.add_argument('--lcd', action='store_true',
+                    help='I2C LCD Display')
+parser.add_argument('--bluetoothheadset', action='store_true',
+                    help='Bluetooth Headset Support')
+parser.add_argument('--bluealsa', action='store_true',
+                    help='BlueAlsa Support')
+parser.add_argument('--magicmirror', action='store_true',
+                    help='RoboCop Magic Mirror Support')
+                    
+args = parser.parse_args()
 
 
 def WelcomeMsg(screen):
@@ -26,4 +52,4 @@ def WelcomeMsg(screen):
 
 
 if __name__ == "__main__":
-    Screen.wrapper(WelcomeMsg)
+Screen.wrapper(WelcomeMsg)
